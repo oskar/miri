1f8b01d37e2ecd54d4e3e90033145abc29f14e38
Oskar Hermansson <oskar.hermansson@gmail.com>
2008-12-06 22:16:37 +0100

    Module for encapsulation

--- miri.rb
+++ miri.rb
@@ -1,75 +1,63 @@
 require 'thread'
 
-def find(name)
-  Thread.list.each do |t|
-    return t if t['name'] == name
-  end
-  return nil
-end
-
-def need(name)
-  loop do
-    return if find(name)
-    Thread.pass
-  end
-end
-
-def send(sig, receiver)
-  t = find(receiver)
-  if t then
-    t['mbox'] ||= Queue.new
-    t['mbox'] << sig
-    Log.log("[#{Thread.current['name']}] Sent signal", sig) if Log.enabled?
-  else
-    Log.log("[#{Thread.current['name']}] Could not send signal", sig) if Log.enabled?
-  end
-end
-
-def receive
-  # Create mbox if missing
-  Thread.current['mbox'] ||= Queue.new
+module Miri
   
-  # Pop first item in queue, blocking call
-  sig = Thread.current['mbox'].pop
+  def self.find(name)
+    Thread.list.each do |t|
+      return t if t['name'] == name
+    end
+    return nil
+  end
 
-  Log.log("[#{Thread.current['name']}] Received signal", sig) if Log.enabled?
-  return sig
-end
+  def self.need(name)
+    loop do
+      return if find(name)
+      Thread.pass
+    end
+  end
 
-class Log
-  @@mutex = Mutex.new
-  @@active = false
-  
-  def self.enable
-    @@active = true
+  def self.send(sig, receiver)
+    t = find(receiver)
+    if t then
+      t['mbox'] ||= Queue.new
+      t['mbox'] << sig
+      log("[#{Thread.current['name']}] Sent signal", sig)
+    else
+      log("[#{Thread.current['name']}] Could not send signal", sig)
+    end
+  end
+
+  def self.receive
+    # Create mbox if missing
+    Thread.current['mbox'] ||= Queue.new
+
+    # Pop first item in queue, blocking call
+    sig = Thread.current['mbox'].pop
+
+    log("[#{Thread.current['name']}] Received signal", sig)
+    return sig
   end
   
-  def self.disable
-    @@active = false
-  end
-  
-  def self.enabled?
-    @@active
-  end
+  @@log_mutex = Mutex.new
   
   def self.log(msg, sig = nil)
     msg += " [#{sig.to_s}]" if sig
-    @@mutex.synchronize do
+    @@log_mutex.synchronize do
       puts msg
     end
   end
+
 end
 
 a = Thread.new do
   Thread.current['name'] = "a"
-  Log.enable
   
-  need("b")
+  Miri::need("b")
   loop do
     sig = "From A to B"
-    send(sig, "b")
+    Miri::send(sig, "b")
     
-    sig = receive()
+    sig = Miri::receive()
     sleep(1)
   end
 end
@@ -77,10 +65,10 @@ end
 b = Thread.new do
   Thread.current['name'] = "b"
   loop do
-    sig = receive()
+    sig = Miri::receive()
 
     sig = "From B to A"
-    send(sig, "a")
+    Miri::send(sig, "a")
   end
 end
 
